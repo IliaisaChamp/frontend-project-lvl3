f590ed719d438f5f4d9fa16611013817bf049f49
Ilia V <ilia.isachamp@gmail.com>
2021-05-11 19:05:26 +0300

    fix

--- src/model.js
+++ src/model.js
@@ -3,10 +3,14 @@ import * as _ from 'lodash';
 import i18next from 'i18next';
 import axios from 'axios';
 import parser from './parser';
+import schema from './utils/validate';
 import Feedback from './Feedback.js';
 import state from './state';
 import ru from './utils/ru';
 import { createFeed, updateFeed } from './views';
+import './scss/index.sass';
+import 'bootstrap/js/dist/modal';
+import 'bootstrap/js/dist/alert';
 
 const getRssData = (url) => {
   const originsUlr = `https://hexlet-allorigins.herokuapp.com/raw?url=${url}`;
@@ -15,7 +19,7 @@ const getRssData = (url) => {
 
 i18next.init({ lng: 'ru', debug: false, resources: { ru } }, (t) => t);
 
-export default (rssUrl, err) => {
+export default () => {
   const feedback = new Feedback();
 
   const watchedFormState = onChange(state, (path, value) => {
@@ -37,44 +41,68 @@ export default (rssUrl, err) => {
     }
   });
 
-  if (err) {
-    watchedFormState.rssForm.state = 'invalid';
-    return;
-  }
-  if (_.has(watchedResponseState.feeds, rssUrl)) {
-    watchedFormState.rssForm.state = 'exist';
-    return;
-  }
-  watchedResponseState.response.state = 'procesing';
+  const model = (rssUrl, err) => {
+    if (err) {
+      watchedFormState.rssForm.state = 'invalid';
+      return;
+    }
+    if (_.has(watchedResponseState.feeds, rssUrl)) {
+      watchedFormState.rssForm.state = 'exist';
+      return;
+    }
+    watchedResponseState.response.state = 'procesing';
 
-  const isEqual = (post1, post2) => (
-    post1.title === post2.title && post1.description === post2.description
-  );
+    const isEqual = (post1, post2) => (
+      post1.title === post2.title && post1.description === post2.description
+    );
 
-  const updateFeeds = (url) => {
-    setTimeout(() => {
-      getRssData(url).then((data) => {
-        const diff = _.differenceWith(data.posts, state.feeds[url].posts, isEqual);
-        if (!_.isEmpty(diff)) {
-          state.feeds[url] = data;
-          watchedResponseState.response.state = 'update';
-          console.log(state.feeds[url].posts);
+    const updateFeeds = (url) => {
+      setTimeout(() => {
+        getRssData(url)
+          .then((data) => {
+            const diff = _.differenceWith(
+              data.posts,
+              state.feeds[url].posts,
+              isEqual,
+            );
+            if (!_.isEmpty(diff)) {
+              state.feeds[url] = data;
+              watchedResponseState.response.state = 'update';
+              console.log(state.feeds[url].posts);
+            }
+          })
+          .finally(() => {
+            updateFeeds(url);
+          });
+      }, 5000);
+    };
+
+    getRssData(rssUrl)
+      .then((data) => {
+        state.feeds[rssUrl] = data;
+        watchedResponseState.response.state = 'success';
+        updateFeeds(rssUrl);
+      })
+      .catch((error) => {
+        if (error) {
+          watchedResponseState.response.state = 'unsuccess';
         }
-      }).finally(() => {
-        updateFeeds(url);
       });
-    }, 5000);
   };
+  const form = document.querySelector('.rss-form');
 
-  getRssData(rssUrl)
-    .then((data) => {
-      state.feeds[rssUrl] = data;
-      watchedResponseState.response.state = 'success';
-      updateFeeds(rssUrl);
-    })
-    .catch((error) => {
-      if (error) {
-        watchedResponseState.response.state = 'unsuccess';
-      }
-    });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const url = formData.get('url');
+    url.trim();
+    schema
+      .validate({ rssUrl: url })
+      .then((resolve) => model(resolve.rssUrl))
+      .catch((error) => {
+        if (error) {
+          model(null, error);
+        }
+      });
+  });
 };
