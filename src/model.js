01d5be49765d95325162272b9905d5fec4b8f8ab
Ilia V <ilia.isachamp@gmail.com>
2021-05-12 16:15:41 +0300

    fix

--- src/model.js
+++ src/model.js
@@ -19,77 +19,77 @@ const getRssData = (url) => {
 
 i18next.init({ lng: 'ru', debug: false, resources: { ru } }, (t) => t);
 
-export default () => {
-  const feedback = new Feedback();
+// export default () => {
+const feedback = new Feedback();
 
-  const watchedFormState = onChange(state, (path, value) => {
-    if (path === 'rssForm.state') {
-      if (value === 'invalid') feedback.showValidMessage(i18next.t('feedback.invalid'), false);
-      if (value === 'exist') feedback.showValidMessage(i18next.t('feedback.exist'), false);
+const watchedFormState = onChange(state, (path, value) => {
+  if (path === 'rssForm.state') {
+    if (value === 'invalid') feedback.showValidMessage(i18next.t('feedback.invalid'), false);
+    if (value === 'exist') feedback.showValidMessage(i18next.t('feedback.exist'), false);
+  }
+});
+
+const watchedResponseState = onChange(state, (path, value) => {
+  if (path === 'response.state') {
+    if (value === 'unsuccess') feedback.showResponseMessage(i18next.t('feedback.unsuccess'), false);
+    if (value === 'success') {
+      feedback.showResponseMessage(i18next.t('feedback.success'), false);
+      createFeed(state.feeds);
     }
-  });
+    if (value === 'procesing') feedback.showResponseMessage(i18next.t('feedback.loading'), true);
+    if (value === 'update') updateFeed(state.feeds);
+  }
+});
 
-  const watchedResponseState = onChange(state, (path, value) => {
-    if (path === 'response.state') {
-      if (value === 'unsuccess') feedback.showResponseMessage(i18next.t('feedback.unsuccess'), false);
-      if (value === 'success') {
-        feedback.showResponseMessage(i18next.t('feedback.success'), false);
-        createFeed(state.feeds);
-      }
-      if (value === 'procesing') feedback.showResponseMessage(i18next.t('feedback.loading'), true);
-      if (value === 'update') updateFeed(state.feeds);
-    }
-  });
+const model = (rssUrl, err) => {
+  if (err) {
+    watchedFormState.rssForm.state = 'invalid';
+    return;
+  }
+  if (_.has(watchedResponseState.feeds, rssUrl)) {
+    watchedFormState.rssForm.state = 'exist';
+    return;
+  }
+  watchedResponseState.response.state = 'procesing';
 
-  const model = (rssUrl, err) => {
-    if (err) {
-      watchedFormState.rssForm.state = 'invalid';
-      return;
-    }
-    if (_.has(watchedResponseState.feeds, rssUrl)) {
-      watchedFormState.rssForm.state = 'exist';
-      return;
-    }
-    watchedResponseState.response.state = 'procesing';
-
-    const isEqual = (post1, post2) => (
-      post1.title === post2.title && post1.description === post2.description
-    );
-    const updateFeeds = (url) => {
-      setTimeout(() => {
-        getRssData(url)
-          .then((data) => {
-            const diff = _.differenceWith(
-              data.posts,
-              state.feeds[url].posts,
-              isEqual,
-            );
-            if (!_.isEmpty(diff)) {
-              state.feeds[url] = data;
-              watchedResponseState.response.state = 'update';
-              console.log(state.feeds[url].posts);
-            }
-          })
-          .finally(() => {
-            updateFeeds(url);
-          });
-      }, 5000);
-    };
-
-    getRssData(rssUrl)
-      .then((data) => {
-        state.feeds[rssUrl] = data;
-        watchedResponseState.response.state = 'success';
-        updateFeeds(rssUrl);
-      })
-      .catch((error) => {
-        if (error) {
-          watchedResponseState.response.state = 'unsuccess';
-        }
-      });
+  const isEqual = (post1, post2) => (
+    post1.title === post2.title && post1.description === post2.description
+  );
+  const updateFeeds = (url) => {
+    setTimeout(() => {
+      getRssData(url)
+        .then((data) => {
+          const diff = _.differenceWith(
+            data.posts,
+            state.feeds[url].posts,
+            isEqual,
+          );
+          if (!_.isEmpty(diff)) {
+            state.feeds[url] = data;
+            watchedResponseState.response.state = 'update';
+            console.log(state.feeds[url].posts);
+          }
+        })
+        .finally(() => {
+          updateFeeds(url);
+        });
+    }, 5000);
   };
 
-  setTimeout(() => {
+  getRssData(rssUrl)
+    .then((data) => {
+      state.feeds[rssUrl] = data;
+      watchedResponseState.response.state = 'success';
+      updateFeeds(rssUrl);
+    })
+    .catch((error) => {
+      if (error) {
+        watchedResponseState.response.state = 'unsuccess';
+      }
+    });
+};
+export default () => {
+  i18next.init({ lng: 'ru', debug: false, resources: { ru } }).then(() => {
     const form = document.querySelector('.rss-form');
 
     form.addEventListener('submit', (e) => {
@@ -106,5 +106,5 @@ export default () => {
           }
         });
     });
-  }, 2000);
+  });
 };
